4a3c51645db3e677c0024a95a5b125e87c61515b
Robert Martinez <robert.david.martinez@gmail.com>
2012-03-19 17:08:23 -0700

    sloppily added rangy capability to input field as well

--- caesar_cipher.js
+++ caesar_cipher.js
@@ -70,28 +70,24 @@ function restoreSelection(containerEl, savedSel) {
     }
 }
 
-function getValue(){
-    var userInput=document.getElementById("messageText").value.toLowerCase();
-    var userInputNoSpaces = userInput.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-    var inputLetters = userInputNoSpaces.split("");
-    var testuserInput=document.getElementById("alsoletterText");
-    var savedSel = saveSelection(testuserInput);
-    console.log(testuserInput)
-    function strip(html)
+function strip(html)
     {
 	var tmp = document.createElement("DIV");
 	tmp.innerHTML = html;
 	return tmp.textContent||tmp.innerText;
     }
-    noteLetters = strip(testuserInput.innerHTML).split("");
+
+function inputFocusGetValue(){
+    var userInput=document.getElementById("messageText");
+    var userInputNoSpaces = userInput.value.toLowerCase().replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+    var inputLetters = userInputNoSpaces.split("");
+    var letterText=document.getElementById("alsoletterText");
+    
+    var savedSel = saveSelection(letterText);
+    
+    noteLetters = strip(letterText.innerHTML).split("");
     console.log(noteLetters);
     console.log(noteLetters.length);
-
-
-    
-   //var userNote=document.getElementById("letterText").value;
-    //var noteLetters = userNote.split("");
-    //console.log(noteLetters);
 
     var alphabet = "abcdefghijklmnopqrstuvwxyz";
     var alphabetLetters = alphabet.split("");
@@ -206,7 +202,137 @@ function getValue(){
     document.getElementById("ciphered-message3").innerHTML = cipheredOutput3;
     document.getElementById("alsoletterText").innerHTML = bestSteganographicNote;
     console.log(bestSteganographicNote);
-    restoreSelection(testuserInput, savedSel);
+    restoreSelection(letterText, savedSel);
+
+
+}
+
+function getValue(){
+    var userInput=document.getElementById("messageText");
+    var userInputNoSpaces = userInput.value.toLowerCase().replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+    var inputLetters = userInputNoSpaces.split("");
+    var letterText=document.getElementById("alsoletterText");
+    
+    var savedSel = saveSelection(userInput);
+    
+    noteLetters = strip(letterText.innerHTML).split("");
+    console.log(noteLetters);
+    console.log(noteLetters.length);
+
+    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+    var alphabetLetters = alphabet.split("");
+	
+    var mostEmbeddedCipher = new Array();
+    var shiftAmount = 0;
+    var bestShiftAmount = 0;
+    var bestCipheredLetters = new Array();
+    var bestCipheredWordCount = 0;
+    var bestSteganographicNote = "";
+    var bestNoteLetters = new Array();
+    var bestUsedNoteLetters = new Array();
+
+    for (n=1; n <= 25; n++){
+	shiftAmount = n;
+	console.log("Shift Amount: " + shiftAmount);
+
+	var originalMessageLetters = new Array();
+	var cipheredLetters = new Array();
+	for (i=0; i < inputLetters.length; i++){
+	    for (j=0; j < alphabetLetters.length; j++){
+		if (inputLetters[i] === " "){
+		    cipheredLetters.push("-");
+		    originalMessageLetters.push("-");
+		    break;
+		}else if(inputLetters[i] === alphabetLetters[j]){
+		    var caesarShiftAmount = (j + shiftAmount)%26;
+		    cipheredLetters.push(alphabetLetters[caesarShiftAmount]);
+		    originalMessageLetters.push(alphabetLetters[j]);
+		}
+	    }
+	}
+	console.log("Ciphered Letters: " + cipheredLetters);
+	
+	var steganographicNote = "";
+	var usedCipherLetters = new Array();
+	var usedNoteLetters = new Array();
+	var cipheredWordCount = 0
+	var k=0;
+	for (i=0; i < cipheredLetters.length; i++){
+	    while (k < noteLetters.length){
+		if (cipheredLetters[i] === "-"){
+		    cipheredWordCount++;
+		    break;
+		}else if(noteLetters[k].toLowerCase() === cipheredLetters[i]){
+		    steganographicNote += "<strong><font color='2F96B4'>" + noteLetters[k] + "</font></strong>";
+		    usedCipherLetters.push(cipheredLetters[i]);
+		    usedNoteLetters.push(i);
+		    k++;
+		    break;
+		}else{
+		    steganographicNote += noteLetters[k];
+		    k++;
+		    usedNoteLetters.push(i);
+		}
+	    }
+	}
+	
+	if (usedCipherLetters.length > mostEmbeddedCipher.length){
+	    mostEmbeddedCipher = usedCipherLetters;
+	    bestCipheredLetters = cipheredLetters;
+	    bestShiftAmount = shiftAmount;
+	    bestCipheredWordCount = cipheredWordCount;
+	    bestSteganographicNote = steganographicNote;
+	    bestNoteLetters = noteLetters;
+	    bestUsedNoteLetters = usedNoteLetters;
+	    //console.log("Used Cipher Letters: " + usedCipherLetters);
+	    //console.log("Most Embedded Cipher: " + mostEmbeddedCipher);
+	    //console.log("Shift Amount: " + shiftAmount);
+	    //console.log("Best Shift Amount: " + bestShiftAmount);
+	    //console.log("Best Ciphered Letters: " + bestCipheredLetters);
+	}
+	
+    }
+    
+    var uppertable = new Array();
+    var lowertable1 = new Array();
+
+    if (mostEmbeddedCipher.length + bestCipheredWordCount < bestCipheredLetters.length){
+	for (i=0; i< mostEmbeddedCipher.length + bestCipheredWordCount; i++){
+	    uppertable.push("<td>" + originalMessageLetters[i] + "</td>");
+	    lowertable1.push("<td><strong><font color='2F96B4'>" + bestCipheredLetters[i] + "</font></strong></td>");
+	}
+	for (i=mostEmbeddedCipher.length + bestCipheredWordCount; i< originalMessageLetters.length; i++){
+	    uppertable.push("<td>" + originalMessageLetters[i] + "</td>");
+	    lowertable1.push("<td>" + bestCipheredLetters[i] + "</td>");
+	}
+	var cipheredOutput1 = "<table class='table table-condensed lead' style='border-top: none'><tr>" + uppertable.join("") + "</tr><tr>" + lowertable1.join("") + "</tr></table>";
+	var cipheredOutput2 = "(Caesar Shifted <font color='2F96B4'>" + bestShiftAmount + "</font> places)";
+	var cipheredOutput3 = "(<font color='2F96B4'>" + mostEmbeddedCipher.length + "</font> of " + (bestCipheredLetters.length - bestCipheredWordCount) + " letters used)";
+
+    }
+
+
+    if (mostEmbeddedCipher.length + bestCipheredWordCount === bestCipheredLetters.length || bestUsedNoteLetters.length < bestNoteLetters.length){
+	var orange = bestUsedNoteLetters.length;
+	var noteRemainder = bestNoteLetters.slice(orange).join("");
+	bestSteganographicNote += noteRemainder;
+	for (i=0; i< bestCipheredLetters.length; i++){
+	    uppertable.push("<td>" + originalMessageLetters[i] + "</td>");
+	    lowertable1.push("<td>" + bestCipheredLetters[i] + "</td>");
+	}
+	var cipheredOutput1 = "<table class='table table-condensed lead' style='border-top: none'><tr>" + uppertable.join("") + "</tr><tr>" + lowertable1.join("") + "</tr></table>";
+	var cipheredOutput2 = "(Caesar Shifted <font color='2F96B4'>" + bestShiftAmount + "</font> places)";
+	var cipheredOutput3 = "<strong><font color='2F96B4'>" + "The Message Fits!" + "</strong></font>";
+    }
+    
+    
+	
+    document.getElementById("ciphered-message1").innerHTML = cipheredOutput1;
+    document.getElementById("ciphered-message2").innerHTML = cipheredOutput2;
+    document.getElementById("ciphered-message3").innerHTML = cipheredOutput3;
+    document.getElementById("alsoletterText").innerHTML = bestSteganographicNote;
+    console.log(bestSteganographicNote);
+    restoreSelection(userInput, savedSel);
 
 
 }
