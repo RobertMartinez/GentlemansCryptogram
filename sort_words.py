9b3f95e72d992de340bb8f9046fab3f24964b794
Robert Martinez <robert.david.martinez@gmail.com>
2014-05-04 17:45:54 -0700

    adding a longer list go words
    to implement later

---
+++ sort_words.py
@@ -0,0 +1,6 @@
+import os
+
+# xs = ['the', 'of']
+xs=['the','of','and','a','to','in','is','you','that','it','he','for','was','on','are','as','with','his','they','at','be','this','from','I','have','or','by','one','had','not','but','what','all','were','when','we','there','can','an','your','which','their','said','if','do','will','each','about','how','up','out','them','then','she','many','some','so','these','would','other','into','has','more','her','two','like','him','see','time','could','no','make','than','first','been','its','who','now','people','my','made','over','did','down','only','way','find','use','may','water','long','little','very','after','words','called','just','where','most','know','get','through','back','much','go','good','new','write','our','me','man','too','any','day','same','right','look','think','also','around','another','came']
+print xs
+print sorted(xs, key=len, reverse=True)
